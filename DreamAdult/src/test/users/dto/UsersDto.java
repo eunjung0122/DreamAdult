fef2dc498d9c0f2cb1664761b9c734540aabd4a4
acorn <acorn@DESKTOP-TO4Q5GJ>
2021-07-01 16:48:16 +0900

    dao, dto 수정

--- DreamAdult/src/test/users/dto/UsersDto.java
+++ DreamAdult/src/test/users/dto/UsersDto.java
@@ -4,48 +4,81 @@ public class UsersDto {
 	//필드
 	private String id;
 	private String pwd;
+	private String nick;
 	private String email;
 	private String profile;
+	private String lang;
 	private String regdate;
+	
 	//디폴트 생성자 
 	public UsersDto() {}
-	public UsersDto(String id, String pwd, String email, String profile, String regdate) {
+	
+	public UsersDto(String id, String pwd, String nick, String email, String profile, String lang, String regdate) {
 		super();
 		this.id = id;
 		this.pwd = pwd;
+		this.nick = nick;
 		this.email = email;
 		this.profile = profile;
+		this.lang = lang;
 		this.regdate = regdate;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getPwd() {
 		return pwd;
 	}
+
 	public void setPwd(String pwd) {
 		this.pwd = pwd;
 	}
+
+	public String getNick() {
+		return nick;
+	}
+
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getProfile() {
 		return profile;
 	}
+
 	public void setProfile(String profile) {
 		this.profile = profile;
 	}
+
+	public String getLang() {
+		return lang;
+	}
+
+	public void setLang(String lang) {
+		this.lang = lang;
+	}
+
 	public String getRegdate() {
 		return regdate;
 	}
+
 	public void setRegdate(String regdate) {
 		this.regdate = regdate;
-	}
+	}	
+	
 	
 }
