824960be5df599ab7fef64e136665f7d038b6833
EUNU <EUNU@10.0.1.5>
2021-07-05 11:09:10 +0900

    댓글 추가기능

--- DreamAdult/WebContent/WEB-INF/table.sql
+++ DreamAdult/WebContent/WEB-INF/table.sql
@@ -35,5 +35,21 @@ CREATE TABLE board_QnA(
    regdate DATE, --글 작성일
    category VARCHAR2(100) NOT NULL
 );
--- 게시글의 번호를 얻어낼 시퀀스
+
 CREATE SEQUENCE board_QnA_seq; 
+
+-- 댓글기능을 생성할 테이블
+CREATE TABLE board_qna_comment(
+   num NUMBER PRIMARY KEY, --댓글의 글번호
+   writer VARCHAR2(100), --댓글 작성자의 아이디
+   nick VARCHAR2(100), --댓글 작성자의 닉네임
+   content VARCHAR2(500), --댓글 내용
+   target_nick VARCHAR2(100), --댓글의 대상자 닉네임
+   ref_group NUMBER, -- 원글의 글번호
+   comment_group NUMBER,
+   deleted CHAR(3) DEFAULT 'no',
+   regdate DATE
+);
+
+-- 댓글기능의 번호를 얻어낼 시퀀스
+CREATE SEQUENCE board_qna_comment_seq;
